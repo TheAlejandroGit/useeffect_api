81177fa00ebeed5302fb1136fbc1c4fc1645a24a
Alejandro Mijangos <43855299+TheAlejandroGit@users.noreply.github.com>
2022-03-07 18:19:13 -0600

    fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from "react";
 
 function App() {
+
+const [weather, setWeather]=useState({});
+const [temperature,setTemperature]=useState(0);
+const [isFh, setIsFh]= useState(true);
+
+
+const success=pos=>{
+
+const latitude= pos.coords.latitude;
+const longitude=pos.coords.longitude;
+  
+axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=24707f74ce4469cea3264d85915485e1`)
+
+.then(res=> {
+  setWeather(res.data)
+  setTemperature(res.data.main.temp)
+
+
+
+});
+
+}
+
+const convertTemp=()=>{
+if(isFh){
+  setTemperature(((temperature-32) *5)/9 );
+  setIsFh(false);
+
+  } else {
+    setTemperature((1.8)*temperature+32);
+    setIsFh(true);
+  }
+
+}
+
+useEffect(()=>{
+
+  navigator.geolocation.getCurrentPosition(success);
+
+},[]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className='weatherCard'>
+<h1>Weather App</h1>
+  {weather.weather ? <img src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt=""/> :null}
+  
+  <ul>
+    <li><b> {weather.name}, {weather.sys?.country}</b></li>
+    <li> <b> {Math.round(temperature)} {isFh ? "Fº" :"Cº"}</b> </li>
+    </ul>
+    <button onClick={convertTemp}>Convert to {isFh ? "Celcius" :"Farenheight"}</button>
+  </div>
+  </div>
   );
 }
 
